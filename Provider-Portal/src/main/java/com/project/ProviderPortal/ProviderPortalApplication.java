2c47ae9ca4ee26665cc3042eaa365496e701a990
Sujan Illuri <sujan@Sujan-Illuri-MacBook-Pro.local>
2022-06-02 09:32:28 -0400

    Initial Spring project

---
+++ Provider-Portal/src/main/java/com/project/ProviderPortal/ProviderPortalApplication.java
@@ -0,0 +1,13 @@
+package com.project.ProviderPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderPortalApplication.class, args);
+	}
+
+}
---
+++ Provider-Portal/src/test/java/com/project/ProviderPortal/ProviderPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ProviderPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
